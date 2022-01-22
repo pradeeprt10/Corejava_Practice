2d2f40f540e840299a92993d80179b461bf18131
Pradeep Rout <you@example.com>
2022-01-22 22:34:50 +0530

    ohh noooo

--- src/main/java/Gitpack/GitClass.java
+++ src/main/java/Gitpack/GitClass.java
@@ -3,6 +3,6 @@ package Gitpack;
 public class GitClass {
 
 	public static void main(String[] args) {
-		System.out.println("hello World");
+		System.out.println("hello World----------------");
 	}
 }
---
+++ src/main/java/Gitpack/MyTest.java
@@ -0,0 +1,10 @@
+package Gitpack;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+	System.out.println("OMM SANTI");
+
+	}
+
+}
