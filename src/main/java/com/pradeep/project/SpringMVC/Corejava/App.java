11519206b6ee7756f8c2922b9733bd59d4500615
Pradeep Rout <you@example.com>
2022-01-22 16:36:50 +0530

    1st  commit

---
+++ src/main/java/com/pradeep/project/SpringMVC/Corejava/App.java
@@ -0,0 +1,21 @@
+package com.pradeep.project.SpringMVC.Corejava;
+
+interface xy{
+	int test();
+}
+
+
+public class App
+{
+	public void test() {
+		final int x;
+		x=90;
+		
+		System.out.println(x);
+	}
+    public static void main( String[] args )
+    {
+     
+    	
+    }
+}
